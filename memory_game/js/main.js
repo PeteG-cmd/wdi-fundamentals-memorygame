2be8835107d9918cdcf7261681a2b7448a794db3
Pete Gilbert <peter.gilbert88@icloud.com>
2020-01-07 13:27:43 +0000

    Created function logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,16 +3,8 @@ console.log("Up and Running!");
 let cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
 
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-
-console.log(cardsInPlay[0]);
-let cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log(cards[2]);
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
+function checkForMatch() {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!")
 	} else {
 		alert("Sorry, try again")
@@ -21,3 +13,14 @@ if (cardsInPlay.length === 2) {
 };
 
 
+function flipCard (cardId) {
+	
+	console.log("Use flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+
+	checkForMatch();
+
+}
+
+flipCard(0);
+flipCard(2);
