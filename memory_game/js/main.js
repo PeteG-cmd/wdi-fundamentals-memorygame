fc969091a25332a76a685617c0336f1ab94c370a
Pete Gilbert <peter.gilbert88@icloud.com>
2020-01-12 00:35:12 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -25,25 +25,58 @@ let cards = [
 let cardsInPlay = [];
 
 function checkForMatch() {
+	
 		if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!")
+		winScore += 1
+		document.getElementById('wins').innerHTML = "Wins: " + winScore;
 	} else {
 		alert("Sorry, try again")
+		lossScore += 1
+		document.getElementById('losses').innerHTML = "Losses: " + lossScore;
 	}
 
 };
 
+function createBoard() {
+	for (let i = 0; i < cards.length; i++) {
+		let cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
 
-function flipCard (cardId) {
+
+function flipCard () {
 	
+	let cardId = this.getAttribute('data-id');
 	console.log("Use flipped " + cards[cardId].rank);
 	console.log(cards[cardId].suit);
 	console.log(cards[cardId].cardImage);
 	cardsInPlay.push(cards[cardId].rank);
 
+	this.setAttribute('src', cards[cardId].cardImage);
+
+	if (cardsInPlay.length === 2) {
 	checkForMatch();
+	}
+}
+
+function resetgame() {
+	cardsInPlay = [];
+	let cardElements = document.getElementsByTagName('img');
+	for (i = 0; i < cardElements.length; i++) {
+		cardElements[i].setAttribute('src', 'images/back.png');
+	}
 
 }
 
-flipCard(0);
-flipCard(2);
+createBoard();
+
+let reset = document.getElementById('button');
+reset.addEventListener('click', resetgame);
+
+let winScore = 0
+let lossScore = 0
